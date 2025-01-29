3ff7b86121dd4803e6af3b8cf6bff5da55afdaad
DruxAMB <druxambmarketing@gmail.com>
2025-01-29 23:42:39 +0100

    modified:   app/events/page.tsx

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,22 +1,19 @@
 "use client";
 
-import ComingSoon from "@/components/ui/coming-soon";
-
-// import EventsHero from "@/components/events/EventsHero";
-// import UpcomingEvents from "@/components/events/UpcomingEvents";
-// import OngoingEvents from "@/components/events/OngoingEvents";
-// import PastEvents from "@/components/events/PastEvents";
-// import EventsCTA from "@/components/events/EventsCTA";
+import EventsHero from "@/components/events/EventsHero";
+import UpcomingEvents from "@/components/events/UpcomingEvents";
+import OngoingEvents from "@/components/events/OngoingEvents";
+import PastEvents from "@/components/events/PastEvents";
+import EventsCTA from "@/components/events/EventsCTA";
 
 export default function EventsPage() {
   return (
     <main className="min-h-screen bg-black">
-      <ComingSoon />
-      {/* <EventsHero />
+      <EventsHero />
       <UpcomingEvents />
       <OngoingEvents />
       <PastEvents />
-      <EventsCTA /> */}
+      <EventsCTA />
     </main>
   );
 }
